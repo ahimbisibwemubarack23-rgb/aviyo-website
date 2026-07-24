43c3ca6d507d4c18cb1545879ee7238372ec3b10
Ahimbisibwe Mubarack <ahimbisibwemubarack23@gmail.com>
2026-07-24 17:21:54 +0000

    Add edge runtime to auth route with workaround

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,6 @@
 // app/api/auth/[...nextauth]/route.ts
+export const runtime = 'edge'
+
 import NextAuth from 'next-auth'
 import CredentialsProvider from 'next-auth/providers/credentials'
 import { supabaseAdmin } from '@/lib/supabase/server'
